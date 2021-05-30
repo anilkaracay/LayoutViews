fe4e700d341e12292b442a7e829ea248bd73b278
anilkaracay <anil.karacay06@hotmail.com>
2021-05-30 18:20:01 +0300

    Initial Commit

---
+++ LayoutViews/LayoutViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutViewsApp.swift
+//  LayoutViews
+//
+//  Created by Macbook Air on 30.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LayoutViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
